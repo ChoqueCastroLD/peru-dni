5350debea0eece8433d0f383480a6ea45e080407
Luis <ghoulhunter@outlook.com.pe>
2019-08-20 01:41:58 -0500

    Fixed typo in index.js and updated  example to readme.md

--- index.js
+++ index.js
@@ -1,72 +1,73 @@
 const fetch = require('node-fetch');
 
 const perudni = {
-    getNameFromDNI(dni, cb) {
+    getNameFromDNI(dni, cb = 0) {
         if (typeof dni !== "string") throw new TypeError("You must pass a String as parameter of getNameFromDNI");
         let DNI = ("00000000" + dni).slice(-8);
         let queryUrl = 'http://aplicaciones007.jne.gob.pe/srop_publico/Consulta/Afiliado/GetNombresCiudadano?DNI=' + DNI;
-        fetch(queryUrl)
-            .then(res => res.text())
-            .then((datos) => {
-                if (datos.length < 100) {
-                    datos = datos.split('|');
-                    cb({
-                        dni: dni,
-                        lastname: datos[0],
-                        secondlastname: datos[1],
-                        name: datos[2]
-                    }, false);
-                } else {
-                    cb({
-                        dni: dni,
-                    }, {
-                        err: true,
-                        message: "Real name not found"
-                    });
-                }
-            })
-            .catch(() => {
-                cb({
-                    dni: dni,
-                }, {
-                    err: true,
-                    message: "Fetch just failed"
-                });
-            })
-    },
-    getNameFromDNI(dni) {
-        if (typeof dni !== "string") throw new TypeError("You must pass a String as parameter of getNameFromDNI");
-        let DNI = ("00000000" + dni).slice(-8);
-        let queryUrl = 'http://aplicaciones007.jne.gob.pe/srop_publico/Consulta/Afiliado/GetNombresCiudadano?DNI=' + DNI;
-        return new Promise((resolve, reject) => {
+        if (cb != 0) {
+            // If callback is set
             fetch(queryUrl)
                 .then(res => res.text())
                 .then((datos) => {
-                    if (datos.length < 100) {
+                    if (!datos.startsWith("|||")) {
                         datos = datos.split('|');
-                        resolve({
+                        cb({
                             dni: dni,
                             fullname: `${datos[2]} ${datos[0]} ${datos[1]}`,
                             lastname: datos[0],
                             secondlastname: datos[1],
                             name: datos[2]
-                        });
+                        }, false);
                     } else {
-                        reject({
+                        datos = datos.split('|||');
+                        cb({}, {
                             dni: dni,
-                            error: true,
-                            message: "Real name not found"
+                            err: true,
+                            message: datos[1]
                         });
                     }
                 })
                 .catch(() => {
-                    reject({
+                    cb({}, {
                         dni: dni,
-                        error: true,
+                        err: true,
                         message: "Fetch just failed"
                     });
                 })
-        })
+        } else {
+            // If callback is not set, return promise
+            return new Promise((resolve, reject) => {
+                fetch(queryUrl)
+                    .then(res => res.text())
+                    .then((datos) => {
+                        if (!datos.startsWith("|||")) {
+                            datos = datos.split('|');
+                            resolve({
+                                dni: dni,
+                                fullname: `${datos[2]} ${datos[0]} ${datos[1]}`,
+                                lastname: datos[0],
+                                secondlastname: datos[1],
+                                name: datos[2]
+                            });
+                        } else {
+                            datos = datos.split('|||');
+                            reject({
+                                dni: dni,
+                                error: true,
+                                message: datos[1]
+                            });
+                        }
+                    })
+                    .catch(() => {
+                        reject({
+                            dni: dni,
+                            error: true,
+                            message: "Fetch just failed"
+                        });
+                    })
+            })
+        }
     }
 }
 
